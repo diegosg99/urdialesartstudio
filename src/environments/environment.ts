d6bf758ef2f3c8de506901c772058a587735f2d9
diegosg99 <diegosg22360@gmail.com>
2023-05-31 10:45:18 +0200

    first commit firebase implemented

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'urdialesartstudio-dc239',
+    appId: '1:374854486574:web:2457b5224b28e9e928edbf',
+    storageBucket: 'urdialesartstudio-dc239.appspot.com',
+    apiKey: 'AIzaSyDx4yB56Xn3HicuPoFWq37xgqFHarGrxms',
+    authDomain: 'urdialesartstudio-dc239.firebaseapp.com',
+    messagingSenderId: '374854486574',
+  },};
